fe41b3bf57db98a66beb048461e653ab23093eda
GitHub Action <action@github.com>
2021-06-26 18:22:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/itwhiteraccoon/goerr/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/itwhiteraccoon/goerr/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.itwhiteraccoon.goerr.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.itwhiteraccoon.goerr.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/itwhiteraccoon/goerr/services/MyProjectService.kt
+++ src/main/kotlin/com/github/itwhiteraccoon/goerr/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.itwhiteraccoon.goerr.services
 
+import com.github.itwhiteraccoon.goerr.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
